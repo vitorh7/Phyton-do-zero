433127a4e48bad9180986a2a74d28bb62a66e38b
antonv1ch <100521424+vitorh7@users.noreply.github.com>
2024-12-27 10:27:05 -0300

    Add files via upload

---
+++ ConversorDeTemperatura.py
@@ -0,0 +1,8 @@
+#Exercício de Lógica de Programação
+#nome: Vitor Pereira
+#data: 26/12/3024
+#pydroid3
+t_celsius = float(input("Digite a temperatura em celsius, utilize ponto para separar casas decimais: "))
+t_farenheit = t_celsius*1.8+32
+t_kelvin = t_celsius+273.15
+print(f"{t_celsius}°C corresponde à {t_farenheit}°F e {t_kelvin}°K")
