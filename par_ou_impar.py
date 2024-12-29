3915e8bd3c086a214a36207ab28642423412c20b
antonv1ch <100521424+vitorh7@users.noreply.github.com>
2024-12-29 15:13:35 -0300

    Update par_ou_impar.py

--- par_ou_impar.py
+++ par_ou_impar.py
@@ -1,9 +1,8 @@
 #Exercício de Lógica de Programação
 #nome: Vitor Pereira
 #data: 24/12/3024
-#phydroid3
+#pydroid3
 
 numero = int(input("Digite um número para ver se é par ou ímpar: ")) #Entrada de dados
-calculo = numero%2 #Resto da divisão por 2
-verificador = calculo==0 #Se houver resto, não é par, logo a igualdade será falsa.
+verificador = numero%2==0 #Se houver resto, não é par, logo a igualdade será falsa.
 print(f"O número que tu digitou é par: {verificador}") #Saída de dados
