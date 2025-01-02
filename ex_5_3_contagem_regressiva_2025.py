16ab4ef8e65e51478d56b7e3af8cdd0cc293c60d
antonv1ch <100521424+vitorh7@users.noreply.github.com>
2025-01-02 14:47:20 -0300

    Update and rename ex5_3_contagem_regressiva_foguete.py to ex_5_3_contagem_regressiva_2025.py

---
+++ ex_5_3_contagem_regressiva_2025.py
@@ -0,0 +1,10 @@
+###################
+# Parte do livro Introdução à Programação com Python (edição 3)
+# Site: https://python.nilo.pro.br/
+# Edicao 3: ex5.5 31/12/2024
+###################
+x=10
+while x>=0:
+	print(x)
+	x=x-1
+print("2025!")
