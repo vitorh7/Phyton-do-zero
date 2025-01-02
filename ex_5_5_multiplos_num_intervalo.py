dfa15548422e797c1e908ce1c3b4f21060f44ab7
antonv1ch <100521424+vitorh7@users.noreply.github.com>
2025-01-02 14:46:00 -0300

    Update and rename ex5_5_multiplo_de_tres_num_intervalo.py to ex_5_5_multiplos_num_intervalo.py

--- ex_5_5_multiplos_num_intervalo.py
+++ ex_5_5_multiplos_num_intervalo.py
@@ -1,7 +1,7 @@
 ###################
 # Parte do livro Introdução à Programação com Python (edição 3)
 # Site: https://python.nilo.pro.br/
-# Edicao 3: ex5.5
+# Edicao 3: ex5.5 30/12/2024
 ###################
 inicio = int(input("Digite um inicio para sequencia: "))
 fim = int(input("Digite um fim para sequencia: "))
